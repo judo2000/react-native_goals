e3448a15bbaeca952773b4574586dcc39dd5837b
Stephen Moore <judo2000@comcast.net>
2022-10-13 14:44:57 -0600

    Move GoalItem into a separate component

--- App.js
+++ App.js
@@ -1,13 +1,6 @@
 import { useState } from 'react';
-import {
-  Button,
-  StyleSheet,
-  Text,
-  TextInput,
-  View,
-  ScrollView,
-  FlatList,
-} from 'react-native';
+import { Button, StyleSheet, TextInput, View, FlatList } from 'react-native';
+import GoalItem from './components/GoalItem';
 
 export default function App() {
   const [enteredGoalText, setEnteredGoalText] = useState('');
@@ -49,12 +42,7 @@ export default function App() {
         <FlatList
           data={courseGoals}
           renderItem={(itemData) => {
-            itemData.index;
-            return (
-              <View style={styles.goalItem}>
-                <Text style={styles.goalText}>{itemData.item.text}</Text>
-              </View>
-            );
+            return <GoalItem text={itemData.item.text} />;
           }}
           // KeyExtractor is useful if you have outside data that does not
           // have a key but does have an id.  Use KeyExtractor to use the
@@ -100,14 +88,5 @@ const styles = StyleSheet.create({
   },
   goalsContainer: {
     flex: 5,
-  },
-  goalItem: {
-    margin: 8,
-    padding: 8,
-    borderRadius: 6,
-    backgroundColor: '#5e08cc',
-  },
-  goalText: {
-    color: 'white',
   },
 });
