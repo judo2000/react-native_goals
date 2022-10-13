be2751e7782a0987606c1f85d725c5716f4575cb
Stephen Moore <judo2000@comcast.net>
2022-10-13 14:23:16 -0600

    Use FlatList to render data rather than mapping through it manually

--- App.js
+++ App.js
@@ -1,5 +1,13 @@
 import { useState } from 'react';
-import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
+import {
+  Button,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+  ScrollView,
+  FlatList,
+} from 'react-native';
 
 export default function App() {
   const [enteredGoalText, setEnteredGoalText] = useState('');
@@ -14,7 +22,7 @@ export default function App() {
     // when state depends on previous state
     setCourseGoals((currentCourseGoals) => [
       ...currentCourseGoals,
-      enteredGoalText,
+      { text: enteredGoalText, key: Math.random().toString() },
     ]);
   }
 
@@ -29,11 +37,40 @@ export default function App() {
         <Button title='Add Goal' onPress={addGoalHandler} />
       </View>
       <View style={styles.goalsContainer}>
-        {courseGoals.map((goal) => (
-          <View style={styles.goalItem} key={goal}>
+        {/* 
+          ScrollView is great for articles that might be long,
+          but FlatList is better for list items because ScrollView
+          renders all the items on the list even if they are not on the screen 
+          which can hurt performance. 
+
+          Flat list will only render the itesm currently on the screen.  All the items
+          off screen will be rendered 'lazily' as the user scrolls.
+        */}
+        <FlatList
+          data={courseGoals}
+          renderItem={(itemData) => {
+            itemData.index;
+            return (
+              <View style={styles.goalItem}>
+                <Text style={styles.goalText}>{itemData.item.text}</Text>
+              </View>
+            );
+          }}
+          // KeyExtractor is useful if you have outside data that does not
+          // have a key but does have an id.  Use KeyExtractor to use the
+          // id as the key for FlatList
+          //
+          // keyExtractor={(item, index) => {
+          //   return item.id;
+          // }}
+          alwaysBounceHorizontal={false}
+        />
+        {/* With FlatList, we no longer map our data manualy */}
+        {/* {courseGoals.map((goal) => ( */}
+        {/* <View style={styles.goalItem} key={goal}>
             <Text style={styles.goalText}>{goal}</Text>
-          </View>
-        ))}
+          </View> */}
+        {/* ))} */}
       </View>
     </View>
   );
