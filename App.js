e0e4690edf67e32deaa139c6200954d325a8b552
Stephen Moore <judo2000@comcast.net>
2022-10-13 15:55:43 -0600

    Add delete function and validation to disallow empty goals

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { StyleSheet, View, FlatList } from 'react-native';
+import { StyleSheet, View, FlatList, KeyExtractor } from 'react-native';
 import GoalItem from './components/GoalItem';
 import GoalInput from './components/GoalInput';
 
@@ -13,9 +13,15 @@ export default function App() {
     // when state depends on previous state
     setCourseGoals((currentCourseGoals) => [
       ...currentCourseGoals,
-      { text: enteredGoalText, key: Math.random().toString() },
+      { text: enteredGoalText, id: Math.random().toString() },
     ]);
   }
+
+  const deleteGoalHandler = (id) => {
+    setCourseGoals((currentCourseGoals) => {
+      return currentCourseGoals.filter((goal) => goal.id !== id);
+    });
+  };
 
   return (
     <View style={styles.appContainer}>
@@ -33,15 +39,21 @@ export default function App() {
         <FlatList
           data={courseGoals}
           renderItem={(itemData) => {
-            return <GoalItem text={itemData.item.text} />;
+            return (
+              <GoalItem
+                text={itemData.item.text}
+                id={itemData.item.id}
+                onDeleteItem={deleteGoalHandler}
+              />
+            );
           }}
           // KeyExtractor is useful if you have outside data that does not
           // have a key but does have an id.  Use KeyExtractor to use the
           // id as the key for FlatList
           //
-          // keyExtractor={(item, index) => {
-          //   return item.id;
-          // }}
+          keyExtractor={(item, index) => {
+            return item.id;
+          }}
           alwaysBounceHorizontal={false}
         />
         {/* With FlatList, we no longer map our data manualy */}
