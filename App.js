58dda0416ac1c4cd569bdb158831340734c3b573
Stephen Moore <judo2000@comcast.net>
2022-10-13 13:37:48 -0600

    Add styling to goalItem

--- App.js
+++ App.js
@@ -30,7 +30,9 @@ export default function App() {
       </View>
       <View style={styles.goalsContainer}>
         {courseGoals.map((goal) => (
-          <Text key={goal}>{goal}</Text>
+          <View style={styles.goalItem} key={goal}>
+            <Text style={styles.goalText}>{goal}</Text>
+          </View>
         ))}
       </View>
     </View>
@@ -61,5 +63,14 @@ const styles = StyleSheet.create({
   },
   goalsContainer: {
     flex: 5,
+  },
+  goalItem: {
+    margin: 8,
+    padding: 8,
+    borderRadius: 6,
+    backgroundColor: '#5e08cc',
+  },
+  goalText: {
+    color: 'white',
   },
 });
